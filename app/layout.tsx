bf5279720569913bcce01b23b1413a177d570189
Your Ajinkya2601 <chitreajinkya15@gmail.com>
2024-10-23 21:55:50 +0530

    video call

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,6 +5,8 @@ import { ClerkProvider } from "@clerk/nextjs";
 import { ToastProvider } from "@/components/providers/toaster-provider";
 import { ConfettiProvider } from "@/components/providers/confetti-provider";
 
+import "@stream-io/video-react-sdk/dist/css/styles.css";
+
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
@@ -18,14 +20,16 @@ export default function RootLayout({
     children: React.ReactNode;
 }) {
     return (
-        <ClerkProvider>
-            <html lang="en">
-                <body className={inter.className}>
-                    <ConfettiProvider />
-                    <ToastProvider />
-                    {children}
-                </body>
-            </html>
-        </ClerkProvider>
+        <html lang="en">
+            <body className={inter.className}>
+                <ClerkProvider>
+                    {/* <StreamVideoProvider> */}
+                        <ConfettiProvider />
+                        <ToastProvider />
+                        {children}
+                    {/* </StreamVideoProvider> */}
+                </ClerkProvider>
+            </body>
+        </html>
     );
 }
